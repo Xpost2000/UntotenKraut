943952eb28297d7c4693854a0b09528a19723939
Xpost2000 <xpost2000cod@gmail.com>
2018-04-02 00:19:45 -0400

    removing debug printing.

--- src/texturemanager.cpp
+++ src/texturemanager.cpp
@@ -4,9 +4,6 @@
 namespace core{
 	std::shared_ptr<TextureManager> TextureManager::instance=nullptr;
 	TextureManager::~TextureManager(){
-		//I'm printing here because I need to make sure std::shared_ptr does what it should be doing
-		std::cout << "Freeing resources..." << std::endl;	
-		//TODO: Implement
 		for(auto& texture : textures){
 			if(texture.second){
 				delete texture.second;
@@ -16,10 +13,8 @@ namespace core{
 
 	gfx::Texture* TextureManager::loadTexture( std::string path ){
 		if(textures.count(path)) { 
-			std::cout << "Valid texture found." << std::endl;
 			return textures[path];
 	       	}else{
-			std::cout << "Loading texture." << std::endl;
 			gfx::Texture* newTexture = new gfx::Texture( path );
 			textures.insert(std::make_pair(path, newTexture));
 			return newTexture;
@@ -28,7 +23,6 @@ namespace core{
 
 	gfx::Texture* TextureManager::getTexture( std::string path ){
 		if(textures.count(path)){
-			std::cout << "Valid texture found." << std::endl;
 			return textures[path];
 		}else{
 			return loadTexture( path );
